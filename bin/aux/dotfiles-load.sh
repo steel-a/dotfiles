902b548f8881d74da3da83d4127ef544de275b44
steel-a <steel4b@gmail.com>
2022-04-17 20:13:45 -0300

    auto updated

--- bin/aux/dotfiles-load.sh
+++ bin/aux/dotfiles-load.sh
@@ -6,8 +6,7 @@ mkdir -p ~/.config/gtk-3.0/	&& ln -f ~/.config/dotfiles/gtk-3.0.settings.ini ~/.
 mkdir -p ~/.config/polybar/     && ln -f ~/.config/dotfiles/polybar.config       ~/.config/polybar/config
 				   ln -f ~/.config/dotfiles/gtkrc-2.0		 ~/.gtkrc-2.0
 
-FILE="~/.config/nitrogen/bg-saved.cfg"
-if [ ! -f "$FILE" ]; then
+if [ ! -f ~/.config/nitrogen/bg-saved.cfg ]; then
   mkdir -p ~/.config/wallpapers/ && ln -f ~/.config/dotfiles/bg.jpg ~/.config/wallpapers/bg.jpg
   mkdir ~/.config/nitrogen
   echo "[xin_-1]"							> ~/.config/nitrogen/bg-saved.cfg
