3e93765b9ad2d3d0eca365c8f6b6fdd0e3cbaf87
steel-a <64929461+steel-a@users.noreply.github.com>
2024-03-06 10:05:42 -0300

    Update pos-install-debian-desktop.sh

--- bin/aux/pos-install-debian-desktop.sh
+++ bin/aux/pos-install-debian-desktop.sh
@@ -103,6 +103,7 @@ if [ -f /root/.install/vnc ]; then
 
   echo '#!/bin/bash'                                                > /root/.install/vnc.sh
   echo "runuser -l $USER -c 'while :; do vncserver -fg --I-KNOW-THIS-IS-INSECURE; done'" >> /root/.install/vnc.sh
+  chmod +x /root/.install/vnc.sh
   
   mv /root/.install/vnc /root/.install/vnc.ok
   echo "VNC config finished"
