6e701c873526c71dc9a5630471ffbba4d0153328
Stella <stell4b@gmail.com>
2024-03-08 12:44:22 +0000

    auto updated

--- bin/aux/pos-install-debian-desktop.sh
+++ bin/aux/pos-install-debian-desktop.sh
@@ -135,7 +135,7 @@ fi
 if [ -f /root/.install/i3 ]; then
 
   echo "Installing i3 packages"
-  apt-get install -y --no-install-recommends i3-wm polybar
+  apt-get install -y --no-install-recommends i3-wm polybar nitrogen
 
   echo "Clonning git repo"
   runuser -l $USER -c 'git clone git@github.com:steel-a/dotfiles.git /home/$USER/.config/dotfiles'
