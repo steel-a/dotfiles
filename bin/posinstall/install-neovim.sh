ab7a2c7d9d02930afc1c37f843fd1d7ec85a5861
steel-a <64929461+steel-a@users.noreply.github.com>
2024-03-09 12:20:21 -0300

    Update install-neovim.sh

--- bin/posinstall/install-neovim.sh
+++ bin/posinstall/install-neovim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install -y --no-install-recommends neovim python3-pip python3-pynvim python3-flake8 python3-bandit python3-isort python3-debugpy python3-venv gcc build-essential
+sudo apt-get install -y neovim python3-pip python3-pynvim python3-flake8 python3-bandit python3-isort python3-debugpy python3-venv gcc build-essential
 touch ~/.bashrc
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
